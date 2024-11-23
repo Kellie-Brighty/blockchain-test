e1188c313339f32c1960f7577ba4c76fb5302db1
Kelly Owoju <kelliebrighty@gmail.com>
2024-11-23 10:29:04 +0100

    title changed

--- src/config.ts
+++ src/config.ts
@@ -1,16 +1,15 @@
 import { http, createConfig } from "wagmi";
-import { base, mainnet, optimism } from "wagmi/chains";
+import { baseSepolia, optimismSepolia } from "wagmi/chains";
 import { injected, metaMask, safe, walletConnect } from "wagmi/connectors";
 import { Project_ID } from "./project.config";
 
 const projectId = Project_ID;
 
 export const config = createConfig({
-  chains: [mainnet, base, optimism],
+  chains: [baseSepolia, optimismSepolia],
   connectors: [injected(), walletConnect({ projectId }), metaMask(), safe()],
   transports: {
-    [mainnet.id]: http(),
-    [base.id]: http(),
-    [optimism.id]: http(),
+    [baseSepolia.id]: http(),
+    [optimismSepolia.id]: http(),
   },
 });
